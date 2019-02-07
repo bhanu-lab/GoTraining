32a088a784dd6110aab58beb0aee54e2fcffa976
blackram <bhanuputtareddy@gmail.com>
2019-02-07 20:36:45 +0530

    changed files to proper package

---
+++ src/github.com/oddeven/OddEvenNumbers.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i <= 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i, " is the even number")
+		} else {
+			fmt.Println(i, " is odd number")
+		}
+	}
+}
