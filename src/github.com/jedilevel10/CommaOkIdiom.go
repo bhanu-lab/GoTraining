65981d36354db3dea3f86721c6f80d986374955a
blackram <bhanuputtareddy@gmail.com>
2019-03-06 22:50:01 +0530

    made changes with comma ok idiom

--- src/github.com/jedilevel10/CommaOkIdiom.go
+++ src/github.com/jedilevel10/CommaOkIdiom.go
@@ -1,21 +1,15 @@
-package main
+package jedilevel10
 
 import "fmt"
 
-func main() {
-
+func OkIdiom() {
 	ch := make(chan int)
 
 	go func() {
 		ch <- 21
-		close(ch)
 	}()
 
 	if v, ok := <-ch; ok == true {
-		fmt.Println("value is ", v, ok)
-	}
-
-	if v, ok := <-ch; ok == false {
 		fmt.Println("value is ", v, ok)
 	}
 }
