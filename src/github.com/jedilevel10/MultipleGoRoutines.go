bf487066f053f25fa50cf2842421cfb3d55d9611
blackram <bhanuputtareddy@gmail.com>
2019-03-11 22:42:59 +0530

    changed logic to get values from channel when using wait group

--- src/github.com/jedilevel10/MultipleGoRoutines.go
+++ src/github.com/jedilevel10/MultipleGoRoutines.go
@@ -1,28 +1,33 @@
-package jedilevel10
+package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 )
+
+var wg sync.WaitGroup
 
 func CreateMultipleRoutines() {
 	ch := make(chan int)
 
-	var wg sync.WaitGroup
-
-	for i := 1; i < 10; i++ {
+	for i := 0; i < 10; i++ { // creates 10 go routines and adds to waitgroup
 		wg.Add(1)
 		go func() {
 			for j := 0; j < 10; j++ {
-				x := i * j
-				ch <- x
+				ch <- j
 			}
-			wg.Done()
+			wg.Done() // indication of go routine is done to main routine
 		}()
 	}
-	wg.Wait()
 
-	for v := range ch {
+	fmt.Println(runtime.NumGoroutine())
+	go func(){
+		wg.Wait()           //wait for all go routines to complete
+		close(ch)           // closing channel after completion of wait fo go routines
+	}
+	for v := range ch { // range can be used since channel is closed
 		fmt.Println(v)
 	}
+	fmt.Println("About to exit program ...")
 }
--- src/github.com/test/sample.go
+++ src/github.com/test/sample.go
@@ -9,6 +9,23 @@ import (
 var wg sync.WaitGroup
 
 func main() {
+	/* ch := make(chan int)
+
+	for i := 0; i < 10; i++ { // creates 10 go routines and adds to waitgroup
+		go func(i int) {
+			for j := 0; j < 10; j++ {
+				ch <- j * i
+			}
+		}(i)
+	}
+
+	fmt.Println(runtime.NumGoroutine())
+
+	for v := 0; v < 100; v++ {
+		fmt.Println(<-ch)
+	}
+	fmt.Println("About to exit program ...") */
+
 	ch := make(chan int)
 
 	for i := 0; i < 10; i++ { // creates 10 go routines and adds to waitgroup
@@ -22,10 +39,13 @@ func main() {
 	}
 
 	fmt.Println(runtime.NumGoroutine())
-	wg.Wait()           //wait for all go routines to complete
-	close(ch)           // closing channel after completion of wait fo go routines
+	go func() {
+		wg.Wait() //wait for all go routines to complete
+		close(ch) // closing channel after completion of wait fo go routines
+	}()
 	for v := range ch { // range can be used since channel is closed
 		fmt.Println(v)
 	}
 	fmt.Println("About to exit program ...")
+
 }
